dc02f2bdd859ec4b350add51b819e4b9b3d7541b
Hassan Tariq <hassan@Hassan-MBP-15.local>
2021-01-28 15:05:44 -0600

    cleaning up minor errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import Dashboard from './containers/Dashboard'
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyles } from './styles/global'
 
+import React, { useContext } from 'react'
+import { ThemeProvider } from 'styled-components'
+import Dashboard from './containers/Dashboard'
+import { lightTheme, darkTheme } from './styles/theme'
+import { GlobalStyles } from './styles/global'
+// import { ThemeContext } from './context/themeContext'
 const App = () => {
   return (
     <div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+// const Container = styled.div`
+//     background-color: ${({ theme }) => theme.secondary};
+//     position: fixed;
+//     left: 0;
+//     top: 0;
+//     bottom: 0;
+//     width: 16rem; `
 
 const Sidebar = () => {
     return (
