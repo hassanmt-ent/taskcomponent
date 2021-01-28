6e0f9f4ecd4f406a76e771b33f59d85eb9223719
Hassan Tariq <hassan@Hassan-MBP-15.local>
2021-01-28 14:53:13 -0600

    changes to the thematic elements of the dashboard will make more updates soon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Dashboard from './containers/Dashboard'
 import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
 
 const App = () => {
   return (
     <div>
       <ThemeProvider />
+      <GlobalStyles />
       <Dashboard/>
       <ThemeProvider />
     </div>
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,18 +1,37 @@
-import { createGlobalStyle } from 'styled-components'
 
-export const GlobalStyles = createGlobalStyle`
-  *,
-  *::after,
-  *::before {
-    box-sizing: border-box;
-  }
-  body {
-    background: ${({ theme }) => theme.primary};
-    color: ${({ theme }) => theme.textColor};
-    height: 100vh;
-    margin: 0;
-    padding: 0;
-    font-family: 'Roboto', sans-serif;
-    transition: all 0.25s linear;
-  }
-  `
+//Creating a front end user friendly system for changing theme elements 
+
+
+const globalTheme = {
+    switchWidth: '40px',
+    switchHeight: '20px',
+    switchPadding: '3px',
+    colorContrastLow: '#d3d3d4',
+    colorWhite: '#FFF',
+    switchColorPrimary: '#302C40',
+    switchAnimationDuration: '0.2s',
+    gradient: 'linear-gradient(122deg, rgba(128,74,216,1) 0%, rgba(98,75,217,1) 100%)',
+    colorGreen: '#5DC399',
+    colorGray: '#adadad',
+}
+
+
+export const lightTheme = {
+    primary: '#FFF',
+    secondary: '#F8F8F8',
+    textColor: '#585280',
+    header: '#585280',
+    headerNumber: '#FFF',
+    activeMenu: '#585280',
+    ...globalTheme
+}
+
+export const darkTheme = {
+    primary: '#302C40',
+    secondary: '#2C2839',
+    textColor: '#FFF',
+    header: '#FFF',
+    headerNumber: '#585280',
+    activeMenu: '#FFF',
+    ...globalTheme
+}
