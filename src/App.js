2078507bf6cd2725bdbc4d32fe23963b19c352d4
Hassan Tariq <hassan@Hassan-MBP-15.local>
2021-01-28 14:46:21 -0600

    globalstyles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Dashboard from './containers/Dashboard'
+import { ThemeProvider } from 'styled-components'
 
 const App = () => {
   return (
     <div>
-      
+      <ThemeProvider />
+      <Dashboard/>
+      <ThemeProvider />
     </div>
   )
 }
---
+++ src/containers/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../components/Main'
+import Sidebar from '../components/Sidebar'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Sidebar />
+            <Main/>
+        </div>
+    )
+}
+
+export default Dashboard
